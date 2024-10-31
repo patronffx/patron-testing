008ab97cf4e9135b5c4183d832d32f72c29306d5
Itzpatron <maximusfadare@gmail.com>
2024-10-31 15:51:23 +0100

    testing

---
+++ settings.js
@@ -0,0 +1,224 @@
+const chalk = require("chalk")
+const fs = require("fs")
+//auto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = false//auto kick spammer (true to on, false to off)
+//===============SETTING MENU==================\\
+global.thumbnail = fs.readFileSync("./data/image/thumb.jpg")
+global.ig = 'justt.patron'
+global.yt = 'Itzpatron1'
+global.ttowner = 'justt.patron'
+global.ownername = 'PATRON 🚹' 
+global.owner = ['2348133729715'] // SETTINGS ARE ALSO IN THE DATABASE FOLDER 
+global.ownernomer = '2348133729715'
+global.socialm = 'GitHub: Itzpatron'
+global.location = 'Nigeria' 
+//========================setting Payment=====================\\
+global.nodana = '8133729715' // IT'S BLANK IF IT'S NOT EXISTING
+global.nogopay = '8133729715' // IT'S BLANK IF IT'S NOT EXISTING 
+global.noovo = '8133729715' // IT'S BLANK IF IT'S NOT EXISTING
+//==================setting Payment Name===========================\\
+global.andana = 'FADARE' // IT'S BLANK IF IT'S NOT EXISTING
+global.angopay = 'FADARE' // IT'S BLANK IF IT'S NOT EXISTING
+global.anovo = 'FADARE' // IT'S BLANK IF IT'S NOT EXISTING
+//==================setting bot===========================\\
+global.botname = "ᴘᴀᴛʀᴏɴ-ᴍᴅ"
+global.ownernumber = '2348133729715'
+global.botnumber = '2348133729715'
+global.ownername = 'ᴘᴀᴛʀᴏɴ-ᴍᴅ🚹'
+global.ownerNumber = ["2348133729715@s.whatsapp.net"]
+global.ownerweb = "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q"
+global.websitex = "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q"
+global.wagc = "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q"
+global.saluran = "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q"
+global.themeemoji = '🚹'
+global.wm = "ᴘᴀᴛʀᴏɴ-ᴍᴅ"
+global.botscript = 'https://github.com/Itzpatron/PATRON-MD' //script link
+global.packname = "Sticker created by"
+global.author = "ᴘᴀᴛʀᴏɴ-ᴍᴅ 🚹"
+global.creator = "2348133729715@s.whatsapp.net"
+global.bankname = "MONIEPOINT"
+global.banknumber = "8133729715"
+global.bankowner = "FADARE"
+//======================== CPANEL COMMAND ===========================\\
+global.domain = '-' // Fill in your domain, don't put a / at the end of the link
+global.apikey = '-' // Fill Apikey
+global.capikey = '-' // Fill Apikey
+//=========================================================//
+//Server create panel egg pm2
+global.apikey2 = '-' // Fill Apikey
+global.capikey2 = '-' // Fill Apikey
+global.domain2 = '-' // Fill Domain
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //don't change it
+
+global.eggsnya2 = '15' // ID of eggs used
+global.location2 = '1' // id location
+//===========================//
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // ID of eggs used
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+//===========================//
+
+global.decor = {
+	menut: '❏═┅═━–〈',
+	menub: '┊•',
+	menub2: '┊',
+	menuf: '┗––––––––––✦',
+	hiasan: '꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷',
+
+	menut: '––––––『',
+    menuh: '』––––––',
+    menub: '┊☃︎ ',
+    menuf: '┗━═┅═━––––––๑\n',
+	menua: '',
+	menus: '☃︎',
+
+	htki: '––––––『',
+	htka: '』––––––',
+	haki: '┅━━━═┅═❏',
+	haka: '❏═┅═━━━┅',
+	lopr: 'Ⓟ',
+	lolm: 'Ⓛ',
+	htjava: '❃'
+}
+
+//===========================//
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            makanancentaur: "🥗",
+            makanangriffin: "🥙",
+            makanankyubi: "🍗",
+            makanannaga: "🍖",
+            makananpet: "🥩",
+            makananphonix: "🧀",
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            naga: "🐉",
+            phonix: "🦅",
+            kyubi: "🦊",
+            griffin: "🦒",
+            centaur: "🎠",
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//new
+global.prefix = ['!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+//media target
+global.thum = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./data/image/thumb.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./data/image/thumb.jpg") //ur error pic
+global.thumb = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.defaultpp = 'https://i.imgur.com/5wsWcjp.jpeg' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+wait: "*_ᴘᴀᴛʀᴏɴ🚹, ᴡᴀɪᴛ!!!._*",
+   success: "ʜᴇʜᴇ, *ɪᴛ ᴡᴀꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ🚹*",
+   on: "ɪᴛ'ꜱ ᴀᴄᴛɪᴠᴇ🚹", 
+   off: "ɪᴛ'ꜱ ᴏꜰꜰ ꜱɪʀ🚹",
+   query: {
+       text: "ᴡʜᴇʀᴇ ɪꜱ ᴛʜᴇ ᴛᴇxᴛ ʙʀᴏ?🚹",
+       link: "ᴡʜᴇʀᴇ'ꜱ ᴛʜᴇ ʟɪɴᴋ?🚹",
+   },
+   error: {
+       fitur: "ʏᴏᴏ ꜱᴏʀʀʏ, ᴇʀʀᴏʀ ꜰᴇᴀᴛᴜʀᴇ, ᴘʟᴇᴀꜱᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ᴛʜᴇ ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ꜱᴏ ɪᴛ ᴄᴀɴ ʙᴇ ꜰɪxᴇᴅ ɪᴍᴍᴇᴅɪᴀᴛᴇʟʏ. 🚹",
+   },
+   only: {
+       group: "ʏᴏᴏ ᴍᴀɴ, ʏᴏᴜ ᴄʀᴀᴢʏ? ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ɪɴ ɢʀᴏᴜᴘꜱ. 🚹",
+       private: "ʙʀᴜʜ ᴡᴛꜰ. ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ. 🚹",
+       owner: "ʏᴏᴏ ʙʀᴏ ɢᴛꜰ. ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ʙʏ ʙᴏᴛ ᴏᴡɴᴇʀꜱ. 🚹",
+       admin: "ʏᴏᴏ ʙʀᴏ ɢᴛꜰ. ᴛʜɪꜱ ꜰᴇᴀᴛᴜʀᴇ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜꜱᴇᴅ ʙʏ ʙᴏᴛ ᴏᴡɴᴇʀꜱ. 🚹",
+       badmin: "ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ꜰɪʀꜱᴛ ɴɪɢɢᴀ. 🚹",
+       premium: "ɴɪɢɢᴀ ᴡᴛꜰ ʏᴏᴜ ᴀɪɴ'ᴛ ᴍʏ ᴏᴡɴᴇʀ. 🚹",
+   }
+}
+ 
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "pk-pIWAlRroXTOAigkWdHcYvmlmgzEQXuoMWbVAaLAVZswSRbEB"
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
